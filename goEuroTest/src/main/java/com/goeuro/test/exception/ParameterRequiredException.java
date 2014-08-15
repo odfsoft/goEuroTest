b52921c044388697a4b86e8755db9a969f98a429
folgerfonseca <folger@datapine.com>
2014-08-15 04:20:12 +0200

    Cleaning project

--- goEuroTest/src/main/java/com/goeuro/test/exception/ParameterRequiredException.java
+++
@@ -1,7 +0,0 @@
-package com.goeuro.test.exception;
-
-public class ParameterRequiredException extends GoEuroException {
-	  public ParameterRequiredException(String message) {
-	        super(message);
-	  }
-}
